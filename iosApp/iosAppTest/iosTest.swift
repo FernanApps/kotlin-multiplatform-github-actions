c6e23b75945981bfc71f47002fed7ddceb9de788
FernanApps <dev.fernan.apps@gmail.com>
2024-03-13 12:45:06 -0500

    change main.ios.kt only test 3

--- iosApp/iosAppTest/iosTest.swift
+++ iosApp/iosAppTest/iosTest.swift
@@ -4,6 +4,8 @@ import shared
 final class iosTest: XCTestCase {
 
     func testPlatformNameIsCorrect() throws {
-        XCTAssertEqual(Main_iosKt.getPlatformName(), "iOS")
+        let platformName = Main_iosKt.getPlatformName()
+        print("Ejecutando la prueba para verificar el nombre de la plataforma... El nombre de la plataforma es: \(platformName)")
+        XCTAssertEqual(platformName, "iOS")
     }
 }
