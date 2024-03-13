88fa4b8dbc74532809464d178690345618eac2cf
FernanApps <dev.fernan.apps@gmail.com>
2024-03-13 13:10:39 -0500

    change main.ios.kt only test 4

--- shared/src/iosMain/kotlin/main.ios.kt
+++ shared/src/iosMain/kotlin/main.ios.kt
@@ -1,6 +1,10 @@
 import androidx.compose.ui.window.ComposeUIViewController
 import platform.UIKit.UIDevice
 
-actual fun getPlatformName(): String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+actual fun getPlatformName(): String {
+    val s =  "iOS"
+    println("Printing :::::::::: XXXXX")
+    return s
+}
 
 fun MainViewController() = ComposeUIViewController { App() }
